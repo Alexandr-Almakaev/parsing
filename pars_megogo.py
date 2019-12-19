fe1c1b3e10e851ac2e991ab6c1793ca92023f9ba
Alexandr Almakaev <59039902+Alexandr-Almakaev@users.noreply.github.com>
2019-12-20 00:07:32 +0300

    Create pars_megogo.py

---
+++ pars_megogo.py
@@ -0,0 +1,30 @@
+from bs4 import BeautifulSoup, SoupStrainer
+import requests as req
+
+
+all_films = []
+link = 'https://megogo.ru'
+stop = '<link rel="canonical" href="http://megogo.ru/ru/films" />'
+fout = open('base_megogo.txt', 'w', encoding='UTF-8')
+
+
+def get_html(k):
+    url = 'https://megogo.ru/ru/films/page_{}'.format(j)
+    return req.get(url)
+
+
+def pars(resp1):
+    soup = BeautifulSoup(resp.text, "html.parser")
+    films = soup.find_all('h3', class_='video-title')
+    for i in films:
+        all_films.append((i.text.strip(), link + i.parent.get('href')))
+        fout.write(i.text.strip() + '#DELIMETER#' + link + i.parent.get('href') + '\n')
+
+
+j = 1
+resp = get_html(j)
+while stop in resp.text and resp.status_code == 200:
+    pars(resp)
+    j += 1
+    resp = get_html(j)
+fout.close()
